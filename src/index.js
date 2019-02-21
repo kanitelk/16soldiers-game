586099d28d8a5813396260672f3d0e664c450060
Kanitelk <derodroll@gmail.com>
2019-02-21 23:22:05 +0300

    add rendering

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./css/style.scss";
+import p5 from "p5";
+//new p5();
 
 let pole = [];
 
@@ -206,7 +208,7 @@ function setDots(pole) {
     lines: [[4, 6], [6, 6], [4, 4], [5, 5]]
   };
   pole[5][7] = {
-    player: 1,
+    player: 0,
     lines: [[4, 6], [4, 7], [6, 8]]
   };
 
@@ -272,5 +274,86 @@ function setDots(pole) {
 
 pole = removeDots(pole);
 pole = setDots(pole);
+
+window.setup = function() {
+  createCanvas(530, 530);
+  background(248, 241, 223);
+};
+
+var newX = 0, newY = 0, clickCounter = 0;
+
+window.draw = function() {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (pole[j][i].player === 2) {
+        fill(246, 25, 36);
+        circle(i * 60 + 20, j * 60 + 20, 8);
+        //stroke(156, 39, 176);
+        pole[j][i].x = i * 60 + 20;
+        pole[j][i].y = j * 60 + 20;
+      }
+      if (pole[j][i].player === 1) {
+        fill(2, 143, 3);
+        circle(i * 60 + 20, j * 60 + 20, 8);
+        //stroke(156, 39, 176);
+        pole[j][i].x = i * 60 + 20;
+        pole[j][i].y = j * 60 + 20;
+      }
+      if (pole[j][i].player === 0) {
+        fill(0, 0, 240);
+        circle(i * 60 + 20, j * 60 + 20, 4);
+        //stroke(156, 39, 176);
+        pole[j][i].x = i * 60 + 20;
+        pole[j][i].y = j * 60 + 20;
+      }
+    }
+  }
+
+  //fill(204, 153, 0);
+  //line(100, 100, 300, 300);
+
+  window.mouseReleased = function() {
+    clickCounter++;
+    console.log(mouseX, mouseY);
+
+    if (clickCounter % 2 == 0) {
+      clickCounter = 0;
+      let X = mouseX;
+      let Y = mouseY;
+
+      for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+          let a = [], b = [];
+          if (
+            pole[j][i].x &&
+            Math.abs(pole[j][i].x - newX) < 20 &&
+            Math.abs(pole[j][i].y - newY) < 20
+          ) {
+            console.log([j, i]);
+          }
+
+          if (
+            pole[j][i].x &&
+            Math.abs(pole[j][i].x - X) < 20 &&
+            Math.abs(pole[j][i].y - Y) < 20
+          ) {
+            pole[j][i].player = 1;
+            console.log([j, i]);
+          }
+      }
+    }
+
+      clear();
+      redraw();
+      background(248, 241, 223);
+    };
+
+    if (clickCounter % 2 == 0) {
+      window.newX = mouseX;
+      window.newY = mouseY;
+    }
+
+  }
+}
 
 console.log(pole);
